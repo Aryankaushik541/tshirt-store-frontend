ad4579e54d1e38c1233a95e00fc35ca99aa7de30
Aryankaushik541 <Aryankaushik541@gmail.com>
2025-12-01 16:47:01 +0530

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetail from './pages/ProductDetail'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import OrderSuccess from './pages/OrderSuccess'
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:slug" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/order-success/:orderNumber" element={<OrderSuccess />} />
+          </Routes>
+        </main>
+        <Footer />
+        <Toaster position="top-right" />
+      </div>
+    </Router>
+  )
+}
+
+export default App
